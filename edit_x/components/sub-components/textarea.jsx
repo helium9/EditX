06816d3022b4bba5990173e9f714ac7b6c48278d
Aadish Jain <aadish.jinesh.jain@gmail.com>
2024-06-02 20:56:20 +0530

    made the inital UI for the text editor area

---
+++ edit_x/components/sub-components/textarea.jsx
@@ -0,0 +1,28 @@
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+export default function InputOutput() {
+  return (
+    <div className="flex flex-row gap-x-4 overflow-auto">
+      <div className="flex flex-col w-full gap-y-4">
+        <Label htmlFor="input" className="m-2">
+          Inputs
+        </Label>
+        <div className="ml-2 overflow-auto max-h-48">
+          <Textarea placeholder="Inputs" id="input" />
+        </div>
+        <Button className="ml-2">Run</Button>
+      </div>
+      <div className="flex flex-col w-full gap-y-4">
+        <Label htmlFor="output" className="m-2">
+          outputs
+        </Label>
+        <div className="mr-2 overflow-auto max-h-48">
+          <Textarea placeholder="Outputs" id="output" disabled />
+        </div>
+        <Button className="mr-2">Copy</Button>
+      </div>
+    </div>
+  );
+}
---
+++ edit_x/components/sub-components/utilityBar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarGroup } from "@nextui-org/react";
+
+export default function UlilityBar() {
+  return (
+    <div className="flex flex-col md:flex-row justify-between w-full gap-x-6 h-16 bg-zinc-800">
+      <div className="flex flex-col md:flex-row justify-start w-full gap-x-6 h-16 bg-zinc-800 p-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex p-2 items-center rounded-md">
+            Font
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="max-h-36">
+            <DropdownMenuItem>10px</DropdownMenuItem>
+            <DropdownMenuItem>12px</DropdownMenuItem>
+            <DropdownMenuItem>14px</DropdownMenuItem>
+            <DropdownMenuItem>16px</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex p-2 items-center rounded-md">
+            Language
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="max-h-72 overflow-auto">
+            <DropdownMenuItem>ABAP</DropdownMenuItem>
+            <DropdownMenuItem>ActionScript</DropdownMenuItem>
+            <DropdownMenuItem>Ada</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button variant="ghost">Push</Button>
+        <Button variant="ghost">Pull</Button>
+        <Button variant="ghost">Run</Button>
+        <Button variant="ghost">Share</Button>
+        <Button variant="ghost">Export</Button>
+      </div>
+      <div className="hidden md:flex flex-col md:flex-row w-full justify-end pr-12">
+        <AvatarGroup isBordered size="sm" max={5}>
+          <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+          <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+          <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+          <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+          <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+          <Avatar src="https://i.pravatar.cc/150?u=a04258114e2902670</AvatarGroup>8c" />
+        </AvatarGroup>
+      </div>
+    </div>
+  );
+}
